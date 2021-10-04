94445dbd69cfa61e5893cca4241f9c16b6f63079
1+ <nilm987521@gmail.com>
2021-10-04 23:18:35 +0800

    Reset Downloda Path

--- ttkan_downloader.py
+++ ttkan_downloader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 from bs4 import BeautifulSoup as bs
@@ -9,28 +9,32 @@ import os
 import re
 import platform
 import sys
+from pathlib import Path
+
+HOME_PATH = str(Path.home())
+DOWNLOADS_DIR  = os.path.join(HOME_PATH,'Downloads')
 
 def get_search_list(keyword:str) -> list:
-    result_list = []
-    url = 'https://www.ttkan.co/novel/search?q={0}'.format(keyword)
-    search_request = get(url)
+    result_list =             []
+    url =                     'https://www.ttkan.co/novel/search?q={0}'.format(keyword)
+    search_request =          get(url)
     search_request.encoding = search_request.apparent_encoding
-    search_request_bs = bs(search_request.text, 'html5lib')
-    novels_list = search_request_bs.find_all('div',{'class':'pure-u-1-1 pure-u-xl-1-3 pure-u-lg-1-3 pure-u-md-1-2 novel_cell'})
+    search_request_bs =       bs(search_request.text, 'html5lib')
+    novels_list =             search_request_bs.find_all('div',{'class':'pure-u-1-1 pure-u-xl-1-3 pure-u-lg-1-3 pure-u-md-1-2 novel_cell'})
     
     for novel in novels_list:
-        info = novel.find_all('li')
+        info =       novel.find_all('li')
         novel_info = {'title': info[0].text,'url' :'https://www.ttkan.co/' + info[0].find('a')['href'], 'writer': info[1].text, 'info':info[2].text}
         result_list.append(novel_info)
     return result_list
 
 
 def get_novel_chapters_list(novel_url:str) -> list:
-    result_list = []
-    chapter_list_request = get(novel_url)
+    result_list =                   []
+    chapter_list_request =          get(novel_url)
     chapter_list_request.encoding = chapter_list_request.apparent_encoding
-    chapter_list_bs = bs(chapter_list_request.text, 'html5lib')
-    chapter_list = chapter_list_bs.find('div', {'class':'full_chapters'}).find_all('a')
+    chapter_list_bs =               bs(chapter_list_request.text, 'html5lib')
+    chapter_list =                  chapter_list_bs.find('div', {'class':'full_chapters'}).find_all('a')
     
     for chapter in chapter_list :
         if re.match(r'https://.*',chapter['href']) != None:
@@ -39,21 +43,22 @@ def get_novel_chapters_list(novel_url:str) -> list:
     return result_list
 
 
-def download_novel(chapters_list:list, store_path:str):
-    if os.path.isdir(store_path) == False : os.mkdir(store_path)
-    index = 1
+def download_novel(chapters_list:list, novel_name:str):
+    DOWNLOAD_PATH =                       os.path.join(DOWNLOADS_DIR, novel_name)
+    if os.path.isdir(DOWNLOAD_PATH) ==    False : os.mkdir(DOWNLOAD_PATH)
+    index =                               1
     for i in tqdm(range(len(chapters_list))):
-        chapter_name = os.path.join(store_path,'{:4d}'.format(index) + '-' + chapters_list[i].text.replace(' ','-')+'.txt')
-        chapter_url = chapters_list[i]['href']
+        chapter_path =                    os.path.join(DOWNLOAD_PATH,'{:4d}'.format(index) + '-' + chapters_list[i].text.replace(' ','-')+'.txt')
+        chapter_url =                     chapters_list[i]['href']
         
-        chapter_request = get(chapter_url)
+        chapter_request =                 get(chapter_url)
         if chapter_request.status_code == 200:
-            chapter_request.encoding = chapter_request.apparent_encoding
+            chapter_request.encoding =    chapter_request.apparent_encoding
             
-            chapter_request_bs = bs(chapter_request.text, 'html5lib')
-            chapter_content = chapter_request_bs.find('div', {'class':'content'}).text.replace('\n','').replace(' ','')
+            chapter_request_bs =          bs(chapter_request.text, 'html5lib')
+            chapter_content =             chapter_request_bs.find('div', {'class': 'content'}).text.replace('\n','').replace(' ','')
                     
-            with open(chapter_name,'w') as file:
+            with open(chapter_path, 'w') as file:
                 file.write(chapter_content)
             file.close()
         index = index + 1
@@ -93,10 +98,10 @@ def command_mode(url: str):
         print('下載已終止，程式即將關閉，請多支持『天天看小說（https://www.ttkan.co）』')   
 
 
-
 def console_mode():
-# console mode
+    # console mode
     print('歡迎使用『天天看小說（https://www.ttkan.co）』下載器')
+    print('預設下載路徑為：{}/小說名稱'.format(DOWNLOADS_DIR))
     try:
         while True: 
             user_input = input("請輸入小說名稱 或 作者名稱（輸入b離開程式）:")  #希望可以增價值些輸入網址後下載
